517e513a18234946d371ae115d2a5ec9b7e1538f
Jesse Thibodeau <96755740+j-thib@users.noreply.github.com>
2022-12-08 00:17:08 -0500

    Add files via upload

---
+++ DQN/test_main.py
@@ -0,0 +1,65 @@
+import random
+import gym
+
+import environments_test
+from dqn import Agent
+from utils_test import plot_learning_curve
+import numpy as np
+from environments_test import Environment
+
+
+env = Environment(n_firms=2, eq_price=5, eq_demand=1000)
+agent = [Agent(gamma=0.99, epsilon=1.0, batch_size=64, n_actions=4, eps_end=0.01, input_dims=[7], lr=0.003) \
+for i in range(env.n_firms)]
+
+score_history = [list(), list()]
+eps_history = list()
+
+np.random.seed(42)
+random.seed(42)
+
+episodes = 100
+steps = 4
+
+for i in range(episodes):
+    score = 0
+    obs = [None, None]
+    act = [None, None]
+    done = False
+    step = 0
+    for j in range(env.n_firms):
+        obs[j] = env.firms[j].reset()[1-j]
+
+    while step < steps:
+
+        for j in range(env.n_firms):
+            prices = []
+            act[j] = agent[j].choose_action(obs[j])
+            prices.append(environments_test.firmEnv.compute_some_stuff(env.firms[j], actions=act)) # write a function to yield prices and call here
+
+        print(prices)
+
+        env.eq_price = env.equilibrium_price(n_firms=2, prices=prices, theta=environments_test.theta)
+        env.eq_demand = env.equilibrium_demand(env.eq_price, K=environments_test.K, M=environments_test.M)
+
+        for j in range(env.n_firms):
+
+            observation_, reward, done, info, _ = env.firms[j].step(act[j], env.eq_demand)
+            score[j] += reward
+            agent[j].store_transition(observation, action, reward, observation_, done)
+            agent[j].learn()
+            observation[j] = observation_
+        scores.append(agent[j].epsilon)
+        eps_history.append(agent[j].epsilon)
+
+        step += 1
+
+    avg_score = np.mean(scores[-100:])
+
+    print('episode ', i, 'score %.2f' % score,
+            'average score %.2f' % avg_score,
+            'epsilon %.2f' % agent.epsilon)
+
+x = [i+1 for i in range(n_games)]
+filename = 'learning_curve.png'
+plot_learning_curve(x, scores, eps_history, filename)
